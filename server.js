305857a11f1e515250115a9ade6433f9e6dc6aaf
Leonardo Garcia <leonardogarciaoficial1@gmail.com>
2024-09-11 12:41:14 -0300

    Versão 2 do socket

--- server.js
+++ server.js
@@ -7,21 +7,33 @@ const io = new Server(server, {cors:'*'})
 
 server.listen(3000,() => console.log('server running at http://localhost:3000'))
 
+const players = {}
+
 io.on('connection', (socket) => {
     console.log(`${socket.id} se conectou no servidor`)
 
-    socket.broadcast.emit('joinInRoom', { 
+    players[socket.id] = { 
         playerName: Math.random() * 10 +"name",
-        id: socket.id
-    } )
+    } 
+
+    socket.broadcast.emit('joinInRoom', players)
 
 
     socket.on('updatePosition', (positionPlayerData) => {
-        socket.broadcast.emit('receivePlayerPosition', {...positionPlayerData})
+        
+        players[socket.id] = {
+            ...positionPlayerData,
+            id: socket.id
+        }
+
+        socket.broadcast.emit('receivePlayerPosition', players[socket.id])
     })
 
+   
     socket.on('disconnect', () => {
         console.log('Usuário disconectado');
+        socket.emit('exitTheRoom', socket.id)
+        delete players[socket.id]
     });
 
 })
